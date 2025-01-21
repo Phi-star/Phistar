b9fe8cb1c0c9f85d2eb7195a42b96552f9c70b9d
Phi-star <saliuphilip8@gmail.com>
2025-01-21 09:56:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+    let selectedPrice;
+    let selectedSessionLink;
+
+    function setBookingOption(price, sessionLink) {
+      selectedPrice = price;
+      selectedSessionLink = sessionLink;
+      alert('You selected a session for $' + price);
+    }
+
+    function validateBooking() {
+      const form = document.getElementById('booking-form');
+      if (form.checkValidity() && selectedPrice) {
+        window.location.href = `https://example.com/book/${selectedSessionLink}`;
+      } else if (!selectedPrice) {
+        alert('Please select a session amount.');
+      } else {
+        alert('Please fill in all required details.');
+      }
+    }
